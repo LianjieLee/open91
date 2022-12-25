aa9833e5f0350509f710097eb9f6829768d0d15e
ghcdn <99530249+LianjieLee@users.noreply.github.com>
2022-12-25 16:47:09 +0800

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import subprocess
 def download(url, name):
     cmd = ['ffmpeg', '-hide_banner', '-i', url, '-c', 'copy', name]
     subprocess.run(cmd)
-    subprocess.run(["ls", "-l", "./"])
+    subprocess.run(["ffmpeg"])
 
 if __name__ == '__main__':
     download('https://cdn77.91p49.com//m3u8/589724/589724.m3u8', './output.mp4')
